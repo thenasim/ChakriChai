84e904fc08a30de51e67b56295d7d0afeedc3af0
Nasim Uddin <23162353+thenasim@users.noreply.github.com>
2021-08-11 10:20:29 +0600

    Academic Repo

---
+++ DAL/AcademicRepo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class AcademicRepo
+    {
+        private static readonly ChakriChaiContext context;
+
+        static AcademicRepo()
+        {
+            context = new ChakriChaiContext();
+        }
+        
+        public static List<Academic> GetAllAcademics(int limit)
+        {
+            return context.Academics.Take(limit).ToList();
+        }
+
+        public static List<Academic> GetAcademicsByEmployee(int employeeId, int limit)
+        {
+            return context.Academics.Where(a => a.EmployeeId == employeeId).Take(limit).ToList();
+        }
+
+        public static void CreateAcademic(Academic a)
+        {
+            context.Academics.Add(a);
+            context.SaveChanges();
+        }
+
+        // TODO: Update Academic
+
+        public static void DeleteAcademic(int id)
+        {
+            var academic = context.Academics.Find(id);
+            context.Academics.Remove(academic);
+            context.SaveChanges();
+        }
+    }
+}
