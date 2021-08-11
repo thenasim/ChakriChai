49decead6659d426c399545708345fd3d0d1d689
Nasim Uddin <23162353+thenasim@users.noreply.github.com>
2021-08-11 10:01:36 +0600

    Jobpost repo

---
+++ DAL/JobPostRepo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class JobPostRepo
+    {
+        private static readonly ChakriChaiContext context;
+
+        static JobPostRepo()
+        {
+            context = new ChakriChaiContext();
+        }
+
+        public static List<JobPost> GetAllJobPosts(int limit)
+        {
+            return context.JobPosts.Take(limit).ToList();
+        }
+
+        public static List<JobPost> GetJobPostsByEmployeer(int employeerId, int limit)
+        {
+            return context.JobPosts.Where(j => j.EmployeerId == employeerId).Take(limit).ToList();
+        }
+
+        public static JobPost GetJobPost(int id)
+        {
+            return context.JobPosts.Find(id);
+        }
+        
+        public static void CreateJobPost(JobPost j)
+        {
+            context.JobPosts.Add(j);
+            context.SaveChanges();
+        }
+
+        // TODO: Update JobPost
+
+        public static void DeleteJobPost(int id)
+        {
+            var job = context.JobPosts.Find(id);
+            context.JobPosts.Remove(job);
+            context.SaveChanges();
+        }
+    }
+}
